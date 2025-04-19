a881308c3c3b8bc48ed27cbdf6c715af1563d8b4
IljaSmirnov <ilsmirnov693n@gmail.com>
2025-04-19 20:23:47 +0300

    upd

--- tasks/all/smirnov_i_radix_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/smirnov_i_radix_sort_simple_merge/src/ops_all.cpp
@@ -112,7 +112,7 @@ bool smirnov_i_radix_sort_simple_merge_all::TestTaskALL::PreProcessingImpl() {
 bool smirnov_i_radix_sort_simple_merge_all::TestTaskALL::ValidationImpl() {
   bool is_valid = true;
   if (world_.rank() == 0) {
-    task_data->inputs_count[0] == task_data->outputs_count[0];
+    is_valid = task_data->inputs_count[0] == task_data->outputs_count[0];
   }
   MPI_Bcast(&is_valid, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
   return is_valid;
@@ -146,7 +146,8 @@ bool smirnov_i_radix_sort_simple_merge_all::TestTaskALL::RunImpl() {
   bool flag;
   std::vector<std::future<std::vector<int>>> ths(max_th);
   for (int i = 0; i < max_th; i++) {
-    ths[i] = std::async(std::launch::async, Sorting, this, i, std::ref(local_mas), max_th);
+    ths[i] = std::async(std::launch::async, &smirnov_i_radix_sort_simple_merge_all::TestTaskALL::Sorting, this, i,
+                        std::ref(local_mas), max_th);
   }
   std::deque<std::vector<int>> firstdq, seconddq;
   for (int i = 0; i < max_th; i++) {
@@ -160,7 +161,8 @@ bool smirnov_i_radix_sort_simple_merge_all::TestTaskALL::RunImpl() {
   std::vector<std::thread> threads(max_th);
   while (flag) {
     for (int i = 0; i < max_th; i++) {
-      threads[i] = std::thread(Merging, this, std::ref(firstdq), std::ref(seconddq), std::ref(mtx));
+      threads[i] = std::thread(&smirnov_i_radix_sort_simple_merge_all::TestTaskALL::Merging, this, std::ref(firstdq),
+                                std::ref(seconddq), std::ref(mtx));
     }
     for (auto &th : threads) {
       th.join();
@@ -179,7 +181,6 @@ bool smirnov_i_radix_sort_simple_merge_all::TestTaskALL::RunImpl() {
   if (!firstdq.empty()) {
     local_res = std::move(firstdq.front());
   }
-  MPI_Barrier(MPI_COMM_WORLD);
   std::deque<std::vector<int>> globdq_A;
   if (rank == 0) {
     std::vector<int> local_sorted;
@@ -208,7 +209,7 @@ bool smirnov_i_radix_sort_simple_merge_all::TestTaskALL::RunImpl() {
     std::deque<std::vector<int>> globdq_B;
     while (flag) {
       for (int i = 0; i < max_th; i++) {
-        threads[i] = std::thread(Merging, this, std::ref(globdq_A), std::ref(globdq_B), std::ref(mtx));
+        threads[i] = std::thread(&smirnov_i_radix_sort_simple_merge_all::TestTaskALL::Merging, this, std::ref(globdq_A), std::ref(globdq_B), std::ref(mtx));
       }
       for (auto &th : threads) {
         th.join();
