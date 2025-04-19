eda640519728088aa264f44a41293d3da95ba6f0
IljaSmirnov <ilsmirnov693n@gmail.com>
2025-04-19 23:29:10 +0300

    aaaaa

--- tasks/all/smirnov_i_radix_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/smirnov_i_radix_sort_simple_merge/src/ops_all.cpp
@@ -252,8 +252,8 @@ bool smirnov_i_radix_sort_simple_merge_all::TestTaskALL::PostProcessingImpl() {
     for (size_t i = 0; i < output_.size(); i++) {
       reinterpret_cast<int *>(task_data->outputs[0])[i] = output_[i];
     }
-    printf("WY %n %n %n\n", reinterpret_cast<int *>(task_data->outputs[0][0]),
-           reinterpret_cast<int *>(task_data->outputs[0][1]), reinterpret_cast<int *>(task_data->outputs[0][2]));
+    printf("WY %n %n %n\n", reinterpret_cast<int *>(task_data->outputs[0])[0],
+           reinterpret_cast<int *>(task_data->outputs[0])[1], reinterpret_cast<int *>(task_data->outputs[0])[2]);
     fflush(stdout);
   }
   return true;
